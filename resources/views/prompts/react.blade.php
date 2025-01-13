d1535673b5e68074103573691b894c6563ae991c
Gustavo Paiva <gustavorpaiva@gmail.com>
2025-01-13 12:31:35 -0300

    improves prompt

--- resources/views/prompts/react.blade.php
+++ resources/views/prompts/react.blade.php
@@ -10,7 +10,7 @@ Question: {{ $question }}
 Thought: your reasoning
 Action: choose one of [{{ implode(', ', $toolNames) }}]
 Action Input: input for the action
-Observation: result of the action
+Observation: result of the action - this will be filled in by the tool DO NOT FILL THIS IN
 Thought: I now know the final answer
 Final Answer: your final answer
 
